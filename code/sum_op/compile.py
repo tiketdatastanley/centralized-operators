fc5be67d5fb6d56f80e17d6e95f9cf6dad579b22
tiketdatastanley <stefanus.setiawan@tiket.com>
2023-11-27 06:28:32 +0000

    update compile

--- code/sum_op/compile.py
+++ code/sum_op/compile.py
@@ -1,9 +1,12 @@
+import argparse
+
 from kfp.components import create_component_from_func
 from src import sum_op
 
 
 if __name__ == "__main__":
-    create_component_from_func(
-        sum_op,
-        output_component_file="./manifests/sum_op.yaml"
-    )
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", required=True)
+    args = parser.parse_args()
+
+    create_component_from_func(sum_op, output_component_file=args.path)
